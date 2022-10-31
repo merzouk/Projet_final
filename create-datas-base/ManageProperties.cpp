a5727f78c46e522ec331da4e6f8d317261fbe2db
Merzouk MENHOUR <merzouk@gmail.com>
2022-10-31 08:56:10 +0100

    adds

---
+++ create-datas-base/ManageProperties.cpp
@@ -0,0 +1,144 @@
+#include "ManageProperties.hpp"
+#include "Logger.hpp"
+
+namespace Manage
+{
+       string ManageProperties::get_value_by_key(string key)
+       {
+              int len = int(key.length());
+              if (len == 0)
+              {
+                     cout << "La cle : " << key << "n'est pas valide " << endl;
+                     return " ";
+              }
+              map<string, string>::iterator it = this->map_properties.begin();
+              for (; it != this->map_properties.end(); it++)
+              {
+                     if (key == it->first)
+                            return it->second;
+              }
+              cout << "La cle " << key << " non trouve " << endl;
+              return " ";
+       }
+
+       string ManageProperties::check_elt_by_key(string key)
+       {
+              int len = int(key.length());
+              if (len == 0)
+              {
+                     cout << "La cle : " << key << "n'est pas valide " << endl;
+                     return " ";
+              }
+              map<string, string>::iterator it = this->map_properties.begin();
+              for (; it != this->map_properties.end(); it++)
+              {
+                     if (key == it->first)
+                            return it->second;
+              }
+              return " ";
+       }
+
+       void ManageProperties::display_map_properties()
+       {
+              if (this->map_properties.size() == 0)
+              {
+                     return;
+              }
+              for (auto itr = this->map_properties.begin(); itr != this->map_properties.end(); ++itr)
+              {
+                     cout << endl << "key : " << itr->first << endl << "Value : "
+                                   << itr->second << endl;
+              }
+       }
+
+       void ManageProperties::delete_value_by_key(string key)
+       {
+              if (check_elt_by_key(key) != " ")
+              {
+                     this->map_properties.erase(key);
+              }
+              else
+              {
+                     cout << "Cle : " << key << endl;
+                     cout << endl
+                          << "Suppression impossible, la cle n'existe pas dans map_properties";
+              }
+       }
+
+       void ManageProperties::add_new_elt(string key, string value)
+       {
+              if (key.length() == 0)
+              {
+                     cout << endl << "Ajout impossible, la cle n'est n'est pas renseignee : "
+                                   << key << endl;
+                     return;
+              }
+              if (value.length() == 0)
+              {
+                     cout << endl
+                                   << "Ajout impossible, la valeur n'est n'est pas renseignee : "
+                                   << value << endl;
+                     return;
+              }
+
+              if (check_elt_by_key(key) == " ")
+              {
+                     this->map_properties[key] = value;
+              }
+              else
+              {
+                     cout << "Cle " << key << endl;
+                     cout << "Ajout impossible, la cle existe dans map_properties " << key
+                                   << endl;
+              }
+       }
+
+       void ManageProperties::tokenize(std::string const &key_value, const char delimiter)
+       {
+              size_t start;
+              size_t end = 0;
+              int i = 0;
+              string key = "";
+              string value = "";
+              while ((start = key_value.find_first_not_of(delimiter, end)) != std::string::npos)
+              {
+                     end = key_value.find(delimiter, start);
+                     if (i == 0)
+                     {
+                            key = key_value.substr(start, end - start);
+                            i++;
+                     }
+                     else
+                     {
+                            value = key_value.substr(start, end - start);
+                     }
+              }
+              add_new_elt(key, value);
+       }
+
+       void ManageProperties::read_contains_properties_file(std::string file_name, const char delimiter)
+       {
+              LOG_INIT_CERR();
+              try
+              {
+                     std::ifstream properties(file_name);
+                     if (properties.is_open())
+                     {
+                            std::string line;
+                            while (std::getline(properties, line))
+                            {
+                                   tokenize(line, delimiter);
+                            }
+                            properties.close();
+                     }
+                     else
+                     {
+                            log(LOG_ERR) << "Erreur pendant la tentative d'ouverture du fichier : " + file_name +"\n";
+                     }
+              }
+              catch(exception &ex)
+              {
+                   log(LOG_ERR) << "Erreur durant l'ouverture du fichier :  "+file_name+"  : "+ std::string(ex.what()) + "\n";
+              }
+       }
+}
--- create-datas-base/create_db.cpp
+++ create-datas-base/create_db.cpp
@@ -3,12 +3,15 @@
 #include <sqlite3.h>
 #include <string>
 
+#include "ManageProperties.hpp"
+
 #include "Logger.hpp"
 
 #include <chrono> //milliseconds
 #include <thread> //sleep_for
 
 using namespace std;
+using namespace Manage;
 
 static int callback(void *NotUsed, int argc, char **argv, char **szColName)
 {
@@ -24,10 +27,25 @@ int main(int argc, char **argv)
 
         if(argc < 2)
         {
-                log(LOG_ERR) <<"Le chemin vers le dossier de la base de donnees est obligatoire\n" ;
+                log(LOG_ERR) <<"Le chemin vers le fichier ressources est obligatoire\n";
                 return 1;
         }
-        string path_db = argv[1];
+
+        string properties_file = argv[1];
+        if(properties_file.size() == 0)
+	 {
+           log(LOG_ERR)  <<  "Le fichier ressources n'est pas renseigne, parametres manquants, connexion impossible : " + properties_file + "\n" ;
+           return 1;
+	 }
+	 ManageProperties * manageProperties = new ManageProperties();
+        manageProperties->read_contains_properties_file(properties_file, ':');
+        if(manageProperties->size_map() == 0)
+        {
+                log(LOG_ERR)  <<   "Aucun parametre trouve dans le fichier ressources, connexion impossible\n";
+                return 1;
+	 }
+
+        string path_db =  manageProperties->get_value_by_key("url_data_base");
         const char * db_name = path_db.c_str();
 
         // open database
@@ -42,9 +60,14 @@ int main(int argc, char **argv)
                 log(LOG_INFO) << "Open database successfully\n";
         }
 
+
+	 string sql_query_create_table =  manageProperties->get_value_by_key("create_table");
+
+	 cout <<"sql_query_create_table : " << sql_query_create_table << endl;
+
         // prepare our sql statements
-        const char *pSQL =
-                        "CREATE TABLE temp_datas(id INTEGER PRIMARY KEY AUTOINCREMENT, identite_capt varchar(50), temperature FLOAT, humidity FLOAT, pressure FLOAT, gyro_x FLOAT, gyro_y FLOAT, gyro_z FLOAT, accel_x FLOAT, accel_y FLOAT, accel_z FLOAT, seconde SMALLINT, minute SMALLINT, heure SMALLINT, jour SMALLINT, mois SMALLINT, annee SMALLINT)";
+        const char *pSQL = sql_query_create_table.c_str();
+                        //"CREATE TABLE temp_datas(id INTEGER PRIMARY KEY AUTOINCREMENT, identite_capt varchar(50), temperature FLOAT, humidity FLOAT, pressure FLOAT, gyro_x FLOAT, gyro_y FLOAT, gyro_z FLOAT, accel_x FLOAT, accel_y FLOAT, accel_z FLOAT, seconde SMALLINT, minute SMALLINT, heure SMALLINT, jour SMALLINT, mois SMALLINT, annee SMALLINT)";
 
         // execute sql
         rc = sqlite3_exec(db, pSQL, callback, 0, &errorMsg);
@@ -66,6 +89,9 @@ int main(int argc, char **argv)
                 log(LOG_INFO) << "Create temp_datas table successfully\n";
                 sqlite3_close(db);
         }
+
+        delete manageProperties;
+        manageProperties = nullptr;
 
         return 0;
 }
