cb382e4c1e995eb0d61241e1d5e08b2be51c0bbc
Merzouk MENHOUR <merzouk@gmail.com>
2022-10-31 17:34:17 +0100

    adds

--- insert-datas-base/main.cpp
+++ insert-datas-base/main.cpp
@@ -15,23 +15,25 @@ int main(int argc, char *argv[])
 {
     // Initiate logger (default name is 'log')
     LOG_INIT_CERR();
-    if(argc == 4)
+    if(argc == 3)
     {
        string path_to_json_file = argv[1];
-       const char * path_data_base = argv[2];
-       string properties_file = argv[3];
+       string properties_file = argv[2];
        if(properties_file.size() == 0)
 	{
-              log(LOG_ERR) << "Le fichier ressources n'est pas renseigne, parametres manquants, connexion impossible : " + std::to_string(port) + "\n";
+              log(LOG_ERR) << "Le fichier ressources n'est pas renseigne, parametres manquants, connexion impossible :\n";
 		return 1;
 	}
        ManageProperties * manageProperties = new ManageProperties();
        manageProperties->read_contains_properties_file(properties_file, ':');
 	if(manageProperties->size_map() == 0)
 	{
-              log(LOG_ERR) << "Aucun parametre trouve dans le fichier ressources, connexion impossible : " + std::to_string(port) + "\n";
+              log(LOG_ERR) << "Aucun parametre trouve dans le fichier ressources, connexion impossible : \n";
               return 1;
 	}
+	string path_db =  manageProperties->get_value_by_key("url_data_base");
+	const char * path_data_base = path_db.c_str();
+
        JsonTools * jsonTools = new JsonTools(manageProperties);
        int res = jsonTools->build_Object_from_json_save(path_to_json_file, path_data_base);
        delete jsonTools;
