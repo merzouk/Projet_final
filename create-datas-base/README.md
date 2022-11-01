c502023137b0ce68177a872edbe9516f1e92582a
Merzouk MENHOUR <merzouk@gmail.com>
2022-11-01 09:31:06 +0100

    add instructions

---
+++ create-datas-base/README.md
@@ -0,0 +1,33 @@
+# Partie création de la base de données
+
+
+```
+Le but de cette partie est la création de la base de données où seront stockées les données. 
+```
+
+```
+Au démarrage, le programme lis le fichier ressources pour obtenir le chemin
+vers la base de données ainsi que la requête de création de la table.
+
+Le programme est indépendant de la base et du contenu de la requête.
+```
+
+## LEs valeurs lues dans le fichier properties :
+
+```
+Au démarrage, le programme lis le fichier ressources pour obtenir le chemin
+vers la base de données ainsi que la requête de création de la table.
+```
+
+```
+url_data_base:
+request_create_table
+```
+
+# Note
+
+```
+Pour plus d'informations, veuillez consulter le fichier values.properties à la racine.
+```
+
+
--- insert-datas-base/main.cpp
+++ insert-datas-base/main.cpp
@@ -18,43 +18,43 @@ int main(int argc, char *argv[])
     LOG_INIT_CERR();
     if(argc == 3)
     {
-       try
-       {
-       string path_to_json_file = argv[1];
-       string properties_file = argv[2];
-       if(properties_file.size() == 0)
-	{
-              log(LOG_ERR) << "Le fichier ressources n'est pas renseigne, parametres manquants, connexion impossible :\n";
-		return 1;
-	}
-       ManageProperties * manageProperties = new ManageProperties();
-       manageProperties->read_contains_properties_file(properties_file, ':');
-	if(manageProperties->size_map() == 0)
-	{
-              log(LOG_ERR) << "Aucun parametre trouve dans le fichier ressources, connexion impossible : \n";
-              return 1;
-	}
-	string path_db =  manageProperties->get_value_by_key("url_data_base");
-	const char * path_data_base = path_db.c_str();
+              try
+              {
+                     string path_to_json_file = argv[1];
+                     string properties_file = argv[2];
+                     if(properties_file.size() == 0)
+                     {
+                            log(LOG_ERR) << "Le fichier ressources n'est pas renseigne, parametres manquants, connexion impossible :\n";
+                            return 1;
+                     }
+                     ManageProperties * manageProperties = new ManageProperties();
+                     manageProperties->read_contains_properties_file(properties_file, ':');
+                     if(manageProperties->size_map() == 0)
+                     {
+                            log(LOG_ERR) << "Aucun parametre trouve dans le fichier ressources, connexion impossible : \n";
+                            return 1;
+                     }
+                     string path_db =  manageProperties->get_value_by_key("url_data_base");
+                     const char * path_data_base = path_db.c_str();
 
-       JsonTools * jsonTools = new JsonTools(manageProperties);
-       int res = jsonTools->build_Object_from_json_save(path_to_json_file, path_data_base);
-       delete jsonTools;
-       jsonTools = nullptr;
-       delete manageProperties;
-       manageProperties = nullptr;
-       log(LOG_INFO) << "Insertion en base de donnees s'est achevee avec succes\n" ;
-       }
-       catch(exception & ex)
-       {
-            log(LOG_ERR) <<"Erreur pendant l'insertion en base de donnees "+std::string(ex.what()) +"\n";
-            return 1;
-       }
+                     JsonTools * jsonTools = new JsonTools(manageProperties);
+                     int res = jsonTools->build_Object_from_json_save(path_to_json_file, path_data_base);
+                     delete jsonTools;
+                     jsonTools = nullptr;
+                     delete manageProperties;
+                     manageProperties = nullptr;
+                     log(LOG_INFO) << "Insertion en base de donnees s'est achevee avec succes\n" ;
+              }
+              catch(exception & ex)
+              {
+                   log(LOG_ERR) << "Erreur pendant l'insertion en base de donnees "+std::string(ex.what()) +"\n";
+                   return 1;
+              }
     }
     else
     {
-        log(LOG_ERR) << "Le fichier source au format json n'est pas renseigne\n" ;
-        return 1;
+              log(LOG_ERR) << "Le fichier source au format json n'est pas renseigne\n" ;
+              return 1;
     }
     return 0;
 }
