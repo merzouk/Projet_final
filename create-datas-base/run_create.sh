ae84cd14011c7fda4a46e9a651eed06a49e4791b
Merzouk MENHOUR <merzouk@gmail.com>
2022-11-03 08:56:15 +0100

    adds

--- create-datas-base/run_create.sh
+++ create-datas-base/run_create.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-g++ -L/usr/include -o "create_db"  *.cpp  -lsqlite3
+#g++ -L/usr/include -o "create_db"  *.cpp  -lsqlite3
 
 if [ "$#" -eq 0 ]; then
         echo "Le chemin vers le fichier ressources est obligatoire"
--- read-table-server-tcp/run_prog_read_datas.sh
+++ read-table-server-tcp/run_prog_read_datas.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$#" -ne 2 ]; then
-        echo "                                Deux parametres sont attendus : "
+           echo "                                Deux parametres sont attendus : "
 	    echo "                                   1) Le numero de port du service Boost::asio sur le server TCP "
 	    echo "                                   2) Le chemin vers le fichier de ressources"
 	    exit 1
@@ -9,7 +9,7 @@ fi
 
 echo "1) build and create executable"
 
-g++ -L /usr/lib/ -L/usr/include  -o "read_db" *.cpp  -lboost_system -lboost_thread -lpthread -lsqlite3
+#g++ -L /usr/lib/ -L/usr/include  -o "read_db" *.cpp  -lboost_system -lboost_thread -lpthread -lsqlite3
 
 echo "2) run executable"
 
